2acfc79e4f2157521d249f5b6a47f92dfd659a07
Amir Kouretchian <amir.kouretchian@gmail.com>
2016-04-02 09:32:33 -0700

    adding GameObjectsTests unit test project, and test stubs for GameRound.AllPlayerTurnsCompleted (which is yet to be implemented)

---
+++ GameObjectsTests/GameRoundTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameObjectsTests
+{
+    [TestClass]
+    public class GameRoundTests
+    {
+        [TestMethod]
+        public void GameRoundTests_AllPlayerTurnsCompleted_ReturnsTrue_WhenKeptDiceEqualsTotalDicePerRound()
+        {
+
+        }
+
+        [TestMethod]
+        public void GameRoundTests_AllPlayerTurnsCompleted_Throws_WhenAGivenPlayerIsNotInThisRound()
+        {
+
+        }
+
+        [TestMethod]
+        public void GameRoundTests_AllPlayerTurnsCompleted_ReturnsFalse_WhenKeptDiceNotEqualToDicePerRound()
+        {
+
+        }
+    }
+}
